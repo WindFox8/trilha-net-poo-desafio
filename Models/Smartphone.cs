4a407f450ec0d2e393134200a577657852a683f8
Rafael Correr <119776059+WindFox8@users.noreply.github.com>
2024-04-12 12:18:14 -0300

    Finished project

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,12 +4,30 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        //Feito
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            //Feito
+            this.Modelo = modelo;
+            this.IMEI = imei;
+            this.Memoria = memoria;
         }
+        // Método para exibir informações do smartphone
+        public void ExibirInfo()
+        {
+            Console.WriteLine($"Número: {Numero}");
+            Console.WriteLine($"Modelo: {Modelo}");
+            Console.WriteLine($"IMEI: {IMEI}");
+            Console.WriteLine($"Memória: {Memoria}GB");
+        }
+
+
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//Feito
+Console.WriteLine("Smatphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo-01", imei: "1111-JKS", memoria: 128);
+nokia.ExibirInfo();
+nokia.Ligar();
+nokia.InstalarAplicativo("GitHub");
+Console.WriteLine();
+Console.WriteLine("DADOS DO PROXIMO APARELHO:");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "2024-512PTBR", modelo: "Modelo-ProMAX", imei: "4052-ERG", memoria: 512);
+iphone.ExibirInfo();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Linkedin");
+
